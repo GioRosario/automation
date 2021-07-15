ccb9b824f3f79d0766de95546bfb1f4e8a705f8a
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-15 00:56:52 -0400

    third commit

--- runner.sh
+++ runner.sh
@@ -1,6 +1,6 @@
+image="conda"
 timestamp=$(date +%Y%m%d%H%M%S)
-docker build . -t conda
-docker tag conda:latest $timestamp
+docker build -t $image:$timestamp .
 docker create -ti --name conda conda bash
 docker cp conda:/build .
 echo $QUAY_PASS > ~/password.txt
@@ -8,5 +8,5 @@ cat ~/password.txt | docker login quay.io --username $QUAY_USER --password-stdin
 docker commit temp quay.io/$QUAY_REPOS
 docker push quay.io/$QUAY_REPOS
 docker rm -f conda
-docker rmi $tag
+docker rmi $image:$timestamp
 docker image rm quay.io/$QUAY_REPOS
