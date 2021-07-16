0a2218d3a4471924e8071f546eb1fe9aebb31034
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-16 13:46:42 -0400

    Updated runner.sh with testing

--- runner.sh
+++ runner.sh
@@ -1,13 +1,30 @@
-docker build . -t conda
-docker create -ti --name temp conda bash
-docker cp temp:/build .
-read -p 'Enter Quay.io username: ' uservar
-read -p 'Enter repository name: ' reposvar
-read -sp 'Enter Quay.io password: ' passvar
-echo $passvar > ~/password.txt
-cat ~/my_password.txt | docker login quay.io --username $uservar --password-stdin
-docker commit temp quay.io/$uservar/$reposvar
-docker push quay.io/$uservar/$reposvar
-docker rm -f temp
-docker image rm conda
-docker image rm quay.io/$uservar/$reposvar
+#!/bin/bash
+
+sudo touch requirements.txt
+user=nsls2
+image="condaforge"
+timestamp=$(date +%Y%m%d%H%M%S)
+#docker build . -t conda --target build
+#docker build . -t temp-image --target export
+docker image build . -t $user/${image}:latest \
+                   -t $user/${image}:${timestamp}
+docker create -ti --name temp-container $user/${image}:latest
+#docker create -ti --name temp-container temp-image bash
+docker cp temp-container:/build .
+echo $QUAY_PASS > ~/password.txt
+cat ~/password.txt | docker login quay.io --username $QUAY_USER --password-stdin
+docker commit temp quay.io/$QUAY_REPOS
+docker push quay.io/$QUAY_REPOS
+docker rm -f temp-container
+docker image rm $user/${image}:latest
+#docker image rm temp-image
+
+#user=nsls2
+#image="condaforge"
+#timestamp=$(date +%Y%m%d%H%M%S)
+#docker image build . -t $user/${image}:latest \
+                  # -t $user/${image}:${timestamp}
+
+#docker run -it --name runner $user/${image}:latest
+#docker run -it prac2.sh
+#docker run -it --name dry-run $user/${image}:latest
