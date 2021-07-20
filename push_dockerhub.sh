fbbe894eb73fbece8882065509fe9dd791b0cc7b
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-20 04:01:49 -0400

    Create push_dockerhub.sh

---
+++ push_dockerhub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+docker tag $timestamp_holder $DOCKER_REPO
+docker push $DOCKER_REPO
+
+#docker rmi $latest_holder
+#docker rmi $timestamp_holder
