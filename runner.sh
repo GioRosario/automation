1ef5a76f2f4468a2d0e08fe87c44831e5cba2564
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-20 03:59:20 -0400

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -3,21 +3,27 @@ set -e
 
 sudo touch requirements.txt
 user=nsls2
-image="condaforge"
+image="nsls2-analysis-2021-1.2"
 timestamp=$(date +%Y%m%d%H%M%S)
 env_name="nsls2-analysis-2021-1.2"
 docker image build . -t $user/${image}:latest \
-                   -t $user/${image}:${timestamp}
+                     -t $user/${image}:${timestamp}
 docker run -it --rm -v $PWD:/build $user/${image}:${timestamp} bash -l /build/export.sh
 sudo chown -v $USER: ${env_name}.tar.gz ${env_name}-sha256sum.txt ${env_name}.yml
 
-docker run -it --name quay-container $user/${image}:latest
-echo $QUAY_PASSWORD > ~/password.txt
-cat ~/password.txt | docker login quay.io --username $QUAY_USERNAME --password-stdin
-docker commit quay-container quay.io/$QUAY_REPO
-docker push quay.io/$QUAY_REPO
+latest_holder="$user/${image}:latest"
+timestamp_holder="$user/${image}:${timestamp}"
+export latest_holder
+export timestamp_holder
 
-docker container rm quay-container
-docker rmi $user/${image}:latest
-docker rmi $user/${image}:${timestamp}
-docker image rm quay.io/$QUAY_USERNAME/$QUAY_REPO
+## Push to Quay
+# chmod +x push_quay.sh
+# ./push_quay.sh
+
+## Push to Dockerhub
+#chmod +x push_dockerhub.sh
+#./push_dockerhub.sh
+
+## Push to GHCR
+# chmod +x push_ghcr.sh
+# ./push_ghcr.sh
