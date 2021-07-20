1c1f1ea5d057cfeaffcc18213eefcd0adef23ed5
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-20 04:00:54 -0400

    Create push_quay.sh

---
+++ push_quay.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker run -it --name extra-container $timestamp_holder
+echo $QUAY_PASSWORD | docker login quay.io -u $QUAY_USERNAME --password-stdin
+docker commit extra-container quay.io/$QUAY_REPO
+docker push quay.io/$QUAY_REPO
+docker rm extra-container
+docker image rm quay.io/$QUAY_REPO
+
+#docker rmi $latest_holder
+#docker rmi $timestamp_holder
