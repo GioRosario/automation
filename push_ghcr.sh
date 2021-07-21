9b6a6914fc884af4297e6390b95efc7e3a140ee8
johngedell <74158598+johngedell@users.noreply.github.com>
2021-07-21 10:54:39 -0400

    Update push_ghcr.sh

--- push_ghcr.sh
+++ push_ghcr.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo $PAT | docker login ghcr.io --username $GIT_USERNAME --password-stdin
-docker tag $timestamp_holder ghcr.io/$GIT_USERNAME/$timestamp_holder
-docker image push ghcr.io/$GIT_USERNAME/$timestamp_holder
+echo $GITHUB_TOKEN | docker login ghcr.io --username $GITHUB_USERNAME --password-stdin
+docker tag $timestamp_holder ghcr.io/$GITHUB_USERNAME/$timestamp_holder
+docker image push ghcr.io/$GITHUB_USERNAME/$timestamp_holder
 
 docker rmi $latest_holder
 docker rmi $timestamp_holder
-docker rmi ghcr.io/$GIT_USERNAME/$timestamp_holders
+docker rmi ghcr.io/$GITHUB_USERNAME/$timestamp_holders
